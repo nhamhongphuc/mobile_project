56f5b5c59ec7d214696a1a48c5787e5b57efb69e
nhamhongphuc <Nhamphuc414@gmail.com>
2022-04-27 22:58:49 +0700

    Update 27/4
    Choosetime layout
    Add task layout

--- ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.index_layout);
+        setContentView(R.layout.choosedate_layout);
     }
 }
