079cad2a2f9686390d31c0887d15f8de380c6ad9
nhamhongphuc <Nhamphuc414@gmail.com>
2022-05-03 11:35:04 +0700

    Update UI 3/5/2022
    choose date, choose time, register ....

---
+++ ToDoApp/app/src/main/java/com/example/todoapp/ChooseDate_Dialog.java
@@ -0,0 +1,25 @@
+package com.example.todoapp;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+public class ChooseDate_Dialog extends Dialog {
+    public Context context;
+    public ChooseDate_Dialog(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.addtask_dialog);
+    }
+}
