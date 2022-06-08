81e8f3d9199d258f3b0431a29b33f28b48f6e7ab
KT <khoatran.yuji@gmail.com>
2022-06-08 15:10:51 +0700

    complete forgot password

--- ToDoApp/app/src/main/java/com/example/todoapp/dialog/ForgotPassword_Dialog.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/dialog/ForgotPassword_Dialog.java
@@ -2,22 +2,80 @@ package com.example.todoapp.dialog;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.todoapp.R;
 import com.example.todoapp.model.Note;
 import com.example.todoapp.model.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ForgotPassword_Dialog extends DialogFragment{
+
+    private EditText ed_email1;
+    private Button btn_sendmail;
+    private String TAG;
+
+    public ForgotPassword_Dialog(){}
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = (getResources().getDisplayMetrics().heightPixels)/2;
+        getDialog().getWindow().setLayout(width, height);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ed_email1 = (EditText) view.findViewById(R.id.ed_email1);
+        btn_sendmail = (Button) view.findViewById(R.id.btn_sendmail);
+
+        btn_sendmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPasswordReset(ed_email1.getText().toString());
+                Toast.makeText(getContext(), "Check your email!", Toast.LENGTH_SHORT).show();
+                getDialog().dismiss();
+            }
+        });
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.forgotpassword_dialog, container);
+    }
+
+    public void sendPasswordReset(String email) {
+        // [START send_password_reset]
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String emailAddress = email;
+
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+        // [END send_password_reset]
+    }
 }
